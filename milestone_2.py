dba4fc9e548314b93e57f3a90dcc2ba6f8e93957
Ben Dodd <ben-dodd@outlook.com>
2024-08-20 15:00:53 +0100

    Milestone 2 Task 4

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,7 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Take a guess: ")
+if len(guess) == 1 and guess in 'abcdefghijklmnopqrstuvwxyz':
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
