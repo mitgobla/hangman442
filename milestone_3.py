7430e22736639d02c700f0907b7027c483695ca8
Ben Dodd <ben-dodd@outlook.com>
2024-08-20 15:10:58 +0100

    Milestone 3 Task 3

--- milestone_3.py
+++ milestone_3.py
@@ -1,16 +1,22 @@
 import random
 
-word_list = ["Apple", "Pineapple", "Orange", "Mango", "Pomegranate", "Strawberry"]
+word_list = ["apple", "pineapple", "orange", "mango", "pomegranate", "strawberry"]
 word = random.choice(word_list)
 
-while True:
-    guess = input("Take a guess with a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+def ask_for_input() -> None:
+    while True:
+        guess = input("Take a guess with a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess: str) -> None:
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+ask_for_input()
