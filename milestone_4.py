62763983f2701ec88801fd54186a6abd2bce030d
Ben Dodd <ben-dodd@outlook.com>
2024-08-20 15:23:23 +0100

    Milestone 4 Task 2

--- milestone_4.py
+++ milestone_4.py
@@ -10,3 +10,22 @@ class Hangman:
         self.num_letters = 0
         self.num_lives = num_lives
         self.list_of_guesses = []
+
+    def check_guess(self, guess: str):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess, {guess} is in the word.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter in the word: ")
+            if not guess.isalpha() and len(guess) != 1:
+                print("Invalid letter. Please enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You've already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+hangman = Hangman(word_list=word_list)
+hangman.ask_for_input()
