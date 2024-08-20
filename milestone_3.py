7bcaa74a7780472b0ec4c2761de6ce70e2341c40
Ben Dodd <ben-dodd@outlook.com>
2024-08-20 15:08:37 +0100

    Milestone 3 Task 2

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,16 @@
+import random
+
+word_list = ["Apple", "Pineapple", "Orange", "Mango", "Pomegranate", "Strawberry"]
+word = random.choice(word_list)
+
 while True:
     guess = input("Take a guess with a letter: ")
     if len(guess) == 1 and guess.isalpha():
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
